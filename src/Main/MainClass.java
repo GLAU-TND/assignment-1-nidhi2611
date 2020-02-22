bb3d40abfc3cee81edce0280c215e59c0c46e62b
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-02-22 18:53:43 +0530

    initial commit:Main defined

---
+++ src/Main/MainClass.java
@@ -0,0 +1,11 @@
+package Main;
+
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to DBC's Contact List App");
+
+    }
+}
