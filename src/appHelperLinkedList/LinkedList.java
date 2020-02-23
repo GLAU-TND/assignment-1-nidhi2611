347947c046b00b340975cc04fdbf95eaca6c77fc
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-02-24 00:58:30 +0530

    function written to add contact at first place

--- src/appHelperLinkedList/LinkedList.java
+++ src/appHelperLinkedList/LinkedList.java
@@ -18,4 +18,15 @@ public class LinkedList {
         }
     }
 
+    public void addFirst(Person person) {
+        Node node = new Node(person);
+        if (isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            node.setNext(first);
+            first = node;
+        }
+    }
+
 }
