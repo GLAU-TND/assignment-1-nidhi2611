a7be8777dfc6e34efa98be716e0456cd0ada6b4d
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-02-24 00:56:44 +0530

    function written to add contact

--- src/appHelperLinkedList/LinkedList.java
+++ src/appHelperLinkedList/LinkedList.java
@@ -6,4 +6,16 @@ public class LinkedList {
     Person person;
     private Node first;
     private Node last;
+
+    public void addLast(Person person) {
+        Node node = new Node(person);
+        if (isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            last.setNext(node);
+            last = node;
+        }
+    }
+
 }
